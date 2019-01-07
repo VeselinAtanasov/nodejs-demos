45a26c0b31717e62a697f4e9a0ab6556b12f5d16
veselin.atanasov@opencode.com <veselin.atanasov@opencode.com>
2019-01-07 13:20:14 +0200

    update readme

--- NodeJs/Nodejs-SoftUni-Revision/L02_mongo-db-and-mongoose-task/mongo-playground-app/controllers/tagController.js
+++ NodeJs/Nodejs-SoftUni-Revision/L02_mongo-db-and-mongoose-task/mongo-playground-app/controllers/tagController.js
@@ -6,24 +6,18 @@ const FileAPI = require('../services/apis/FileAPI');
 
 module.exports = (req, res) => {
   if (req.pathname === '/generateTag' && req.method === 'POST') {
-<<<<<<< HEAD
-=======
     let fileAPI = new FileAPI(req, res);
->>>>>>> c9c7d3af29d33a41f67c0295de722c2cf976f164
     let form = new formidable.IncomingForm();
     form.parse(req, (err, fields, files) => {
       if (err) {
         console.log(err);
         return;
       }
-<<<<<<< HEAD
-      console.log(fields, files);
-=======
+
       tagService.addTag(fields).then(data => {
         fileAPI.redirect('/');
       })
         .catch(err => console.log(err));
->>>>>>> c9c7d3af29d33a41f67c0295de722c2cf976f164
     });
   } else {
     return true;
